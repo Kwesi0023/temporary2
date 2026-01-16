e0db2275b651385efccdca952db57dc2649e8759
Kwesi0023 <humphreykusi@gmail.com>
2026-01-16 15:55:57 +0000

    changed somethings on it

--- shared.go
+++ shared.go
@@ -2,9 +2,10 @@ package main
 
 import "strings"
 
-func ValUserInput(firstname string, lastname string, mail string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+func ValUserInput(firstname string, lastname string, mail string, userTickets uint, remainingTickets uint) (bool, bool, bool) { // check if both names are at least two characters long
 	isValidName := len(firstname) >= 2 && len(lastname) > 2
 	isValidEmail := strings.Contains(mail, "@")
 	isValidTicket := userTickets > 0 && userTickets <= remainingTickets
+	// MAtch the order : Name, Email, Ticket
 	return isValidEmail, isValidName, isValidTicket
 }
